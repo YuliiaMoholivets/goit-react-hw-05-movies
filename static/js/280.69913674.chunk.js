3d54b6770659cd2c88da6909e983b465a5407c06
YuliiaMoholivets <112686109+YuliiaMoholivets@users.noreply.github.com>
2023-02-19 06:45:03 +0000

    Deploying to gh-pages from @ YuliiaMoholivets/goit-react-hw-05-movies@f424c68c24c6369dc57fbb92d1d785628ae1eb59 🚀

---
+++ static/js/280.69913674.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[280],{280:function(e,t,n){n.r(t);var r=n(791),o=n(689),c=n(87),s=n(184);t.default=function(){var e=(0,o.s0)();return(0,r.useEffect)((function(){e("/")}),[]),(0,s.jsxs)("p",{children:["Page is not found. Go ",(0,s.jsx)(c.rU,{to:"/",children:"Home"})]})}}}]);
+//# sourceMappingURL=280.69913674.chunk.js.map
